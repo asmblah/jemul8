4075639f68d2cc72a81891ba0da9fa9716e6bbbf
Daniel Phillimore <dan@ovms.co>
2013-04-17 17:41:25 +0100

    Converted tabs to spaces in Register class

--- js/core/classes/register.js
+++ js/core/classes/register.js
@@ -12,58 +12,58 @@
 
 /*global define */
 define([
-	"js/util",
-	"./decoder/register"
+    "js/util",
+    "./decoder/register"
 ], function (
-	util,
-	DecoderRegister
+    util,
+    DecoderRegister
 ) {
     "use strict";
 
-	// Register (eg. CPU registers EAX, EBX) class constructor
-	//	(NB: also used by I/O devices eg. CMOS)
-	function Register(name, size) {
-		DecoderRegister.call(this, name, size);
+    // Register (eg. CPU registers EAX, EBX) class constructor
+    //  (NB: also used by I/O devices eg. CMOS)
+    function Register(name, size) {
+        DecoderRegister.call(this, name, size);
 
-		if (!size) {
-			size = 0;
-		}
+        if (!size) {
+            size = 0;
+        }
 
-		this.value = 0;
-		this.mask = util.generateMask(size);
-	}
+        this.value = 0;
+        this.mask = util.generateMask(size);
+    }
 
-	util.inherit(Register).from(DecoderRegister);
+    util.inherit(Register).from(DecoderRegister);
 
-	util.extend(Register.prototype, {
-		clear: function () {
-			this.set(0x00);
-		},
+    util.extend(Register.prototype, {
+        clear: function () {
+            this.set(0x00);
+        },
 
-		get: function () {
-			return this.value;
-		},
+        get: function () {
+            return this.value;
+        },
 
-		// Returns a nicely formatted hex string, with register value, padded to its size
-		getHexString: function () {
+        // Returns a nicely formatted hex string, with register value, padded to its size
+        getHexString: function () {
             /*jslint bitwise: true */
 
-			var val = (this.get() >>> 0).toString(16).toUpperCase(),
-				sizeHexChars = this.getSize() * 2,
-				textLeadingZeroes = new Array(sizeHexChars - val.length + 1).join("0"),
-				// Use spaces to right-align hex characters with the full 32-bit ones (8 chars)
-				textLeadingSpaces = new Array(8 - sizeHexChars + 1).join(" ");
+            var val = (this.get() >>> 0).toString(16).toUpperCase(),
+                sizeHexChars = this.getSize() * 2,
+                textLeadingZeroes = new Array(sizeHexChars - val.length + 1).join("0"),
+                // Use spaces to right-align hex characters with the full 32-bit ones (8 chars)
+                textLeadingSpaces = new Array(8 - sizeHexChars + 1).join(" ");
 
-			return textLeadingSpaces + textLeadingZeroes + val;
-		},
+            return textLeadingSpaces + textLeadingZeroes + val;
+        },
 
-		set: function (val) {
+        set: function (val) {
             /*jslint bitwise: true */
 
-			// Mask out bits of value outside Register's bit-width
-			this.value = (val & this.mask) >>> 0;
-		}
-	});
+            // Mask out bits of value outside Register's bit-width
+            this.value = (val & this.mask) >>> 0;
+        }
+    });
 
-	return Register;
+    return Register;
 });
