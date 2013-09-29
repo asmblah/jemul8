4d9aae23990107fa4a6ccab9d07170c87d413b39
Daniel Phillimore <dan@ovms.co>
2013-09-29 22:14:08 +0100

    Moved common tools for I/O device testing out to an include

--- asm/tests/bdd/acceptance/real-mode/IODevice/CMOS.asm
+++ asm/tests/bdd/acceptance/real-mode/IODevice/CMOS.asm
@@ -5,46 +5,15 @@
 ; MODULE: Tests for CMOS I/O device
 ;
 
-TEST_PORT equ 0x404
+%include 'tools.inc'
 
-org 0x0100
+section .data:
 
-cli
+; Test descriptions
+test1 db "should return 0xFF when reading index port 0x70",0
+test2 db "should return 0x04 when reading minute register when minute is 4",0
 
-; Create stack
-mov [axBackup], ax
-mov ax, 0x0000
-mov ss, ax
-mov sp, 0xFFFD
-mov ax, [axBackup]
-
-; ======== Block macros ========
-; From http://www.nasm.us/doc/nasmdoc4.html
-%macro if 1
-    %push if
-    j%-1 %$ifnot
-%endmacro
-%macro else 0
-    %ifctx if
-        %repl else
-        jmp %$ifend
-        %$ifnot:
-    %else
-        %error "expected `if' before `else'"
-    %endif
-%endmacro
-%macro endif 0
-    %ifctx if
-        %$ifnot:
-        %pop
-    %elifctx else
-        %$ifend:
-        %pop
-    %else
-        %error "expected `if' or `else' before `endif'"
-    %endif
-%endmacro
-; ======== /Block macros ========
+section .code:
 
 ; Ready to begin testing
 cmp ax, 0
@@ -95,49 +64,3 @@ if e
 endif
 call finished
 jmp done
-
-; Passed a test
-pass:
-mov ax, 0
-ret
-
-; Failed a test
-fail:
-mov ax, 1
-ret
-
-; Finished all tests
-finished:
-mov ax, 2
-ret
-
-; Done
-done:
-hlt
-jmp done
-
-get_length:
-xor cx, cx
-push si
-.next_char
-cmp byte [ds:si], 0
-if ne
-    inc si
-    inc cx
-    jmp .next_char
-endif
-pop si
-ret
-
-print_description:
-call get_length
-mov dx, TEST_PORT
-repe outsb
-ret
-
-; Data
-axBackup dw 0x0000
-
-; Test descriptions
-test1 db "should return 0xFF when reading index port 0x70",0
-test2 db "should return 0x04 when reading minute register when minute is 4",0
