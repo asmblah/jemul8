134dad7c95b086b8ae457e317a33c1ce4fdec76d
Daniel Phillimore <dan@ovms.co>
2013-10-19 23:18:15 +0100

    Added basic tests for "push" instruction

--- tests/bdd/acceptance/real-mode/CPU/Instruction/push-Test.js
+++ tests/bdd/acceptance/real-mode/CPU/Instruction/push-Test.js
@@ -9,15 +9,82 @@
 
 /*global define */
 define([
-    "module",
-    "./tools"
+    "js/util",
+    "tools/TestSystem"
 ], function (
-    module,
-    tools
+    util,
+    TestSystem
 ) {
     "use strict";
 
-    var callback = module.defer();
+    describe("CPU 'push' instruction", function () {
+        var system,
+            testSystem;
 
-    tools.defineTest("push", callback);
+        beforeEach(function (done) {
+            testSystem = new TestSystem();
+            system = testSystem.getSystem();
+
+            testSystem.init().done(function () {
+                done();
+            });
+        });
+
+        describe("when an 8-bit value is pushed", function () {
+            beforeEach(function (done) {
+                var assembly = util.heredoc(function (/*<<<EOS
+mov ax, 0
+mov ss, ax
+mov esp, 0xfffd
+;; Push an 8-bit value: it should be zero-extended to 16-bit
+push 0x12
+hlt
+EOS
+*/) {});
+
+                testSystem.execute(assembly).done(function () {
+                    done();
+                }).fail(function (exception) {
+                    done(exception);
+                });
+            });
+
+            it("should decrease (E)SP by 2", function () {
+                expect(system.getCPURegisters().esp.get()).to.equal(0xfffb);
+            });
+
+            it("should not change SS", function () {
+                expect(system.getCPURegisters().ss.get()).to.equal(0);
+            });
+        });
+
+        describe("when an 16-bit value is pushed", function () {
+            beforeEach(function (done) {
+                var assembly = util.heredoc(function (/*<<<EOS
+mov ax, 0
+mov ss, ax
+mov esp, 0xfffd
+;; 16-bit value cannot be immediate
+mov ax, 0x1234
+push ax
+hlt
+EOS
+*/) {});
+
+                testSystem.execute(assembly).done(function () {
+                    done();
+                }).fail(function (exception) {
+                    done(exception);
+                });
+            });
+
+            it("should decrease (E)SP by 2", function () {
+                expect(system.getCPURegisters().esp.get()).to.equal(0xfffb);
+            });
+
+            it("should not change SS", function () {
+                expect(system.getCPURegisters().ss.get()).to.equal(0);
+            });
+        });
+    });
 });
