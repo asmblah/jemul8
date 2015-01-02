6f9bef108d9212f7c4c5ccafeb120ac854b4991d
Daniel Phillimore <dan@ovms.co>
2015-01-02 00:31:25 +0000

    Added test for CPU 'xchg' instruction with reg<->mem

---
+++ tests/bdd/acceptance/real-mode/CPU/Instruction/xchg-Test.js
@@ -0,0 +1,126 @@
+/**
+ * jemul8 - JavaScript x86 Emulator
+ * http://jemul8.com/
+ *
+ * Copyright 2013 jemul8.com (http://github.com/asmblah/jemul8)
+ * Released under the MIT license
+ * http://jemul8.com/MIT-LICENSE.txt
+ */
+
+/*global define */
+define([
+    "js/util",
+    "tools/TestSystem"
+], function (
+    util,
+    TestSystem
+) {
+    "use strict";
+
+    describe("CPU 'xchg' instruction", function () {
+        /*jshint bitwise: false */
+        var registers,
+            system,
+            testSystem;
+
+        beforeEach(function (done) {
+            testSystem = new TestSystem();
+            system = testSystem.getSystem();
+            registers = system.getCPURegisters();
+
+            testSystem.init().done(function () {
+                done();
+            });
+        });
+
+        afterEach(function () {
+            system.stop();
+            registers = null;
+            system = null;
+            testSystem = null;
+        });
+
+        util.each({
+            "xchg di,[bx+si+2]": {
+                is32BitCodeSegment: false,
+                operand1: "di",
+                operand2: "[bx+si+2]",
+                registers: {
+                    ds: 0x200,
+                    bx: 0x10,
+                    si: 0x20,
+                    di: 0x6789
+                },
+                memory: [{
+                    to: (0x200 << 4) + 0x10 + 0x20 + 2,
+                    data: 0x3456,
+                    size: 2
+                }],
+                expectedRegisters: {
+                    ds: 0x200,
+                    bx: 0x10,
+                    si: 0x20,
+                    di: 0x3456
+                },
+                expectedMemory: [{
+                    from: (0x200 << 4) + 0x10 + 0x20 + 2,
+                    size: 2,
+                    expected: 0x6789
+                }]
+            }
+        }, function (scenario, description) {
+            var is32BitCodeSegment = scenario.is32BitCodeSegment;
+
+            describe("when code segment is " + (is32BitCodeSegment ? 32 : 16) + "-bit", function () {
+                describe(description, function () {
+                    beforeEach(function (done) {
+                        var assembly = util.heredoc(function (/*<<<EOS
+org 0x100
+[BITS ${bits}]
+xchg ${operand1}, ${operand2}
+
+hlt
+EOS
+*/) {}, {operand1: scenario.operand1, operand2: scenario.operand2, bits: is32BitCodeSegment ? 32 : 16});
+
+                        testSystem.on("pre-run", function () {
+                            registers.cs.set32BitMode(is32BitCodeSegment);
+                        });
+
+                        if (scenario.setup) {
+                            scenario.setup(registers);
+                        }
+
+                        util.each(scenario.registers, function (value, register) {
+                            registers[register][/f$/.test(register) ? "setBin" : "set"](value);
+                        });
+
+                        util.each(scenario.memory, function (options) {
+                            system.write(options);
+                        });
+
+                        testSystem.execute(assembly).done(function () {
+                            done();
+                        }).fail(function (exception) {
+                            done(exception);
+                        });
+                    });
+
+                    util.each(scenario.expectedRegisters, function (expectedValue, register) {
+                        it("should set " + register + " correctly", function () {
+                            expect(registers[register].get()).to.equal(expectedValue);
+                        });
+                    });
+
+                    util.each(scenario.expectedMemory, function (data) {
+                        var expectedValue = data.expected;
+
+                        it("should store " + util.hexify(expectedValue) + " at " + util.hexify(data.from), function () {
+                            expect(system.read(data)).to.equal(expectedValue);
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
